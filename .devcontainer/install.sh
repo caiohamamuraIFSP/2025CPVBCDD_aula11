bb36441d0fadcaedd06668cd66049f8f879786b8
caiohamamuraIFSP <hamamura.caio@ifsp.edu.br>
2025-05-06 12:52:49 -0300

    Automate devcontainer

---
+++ .devcontainer/install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install mariadb-server
+sudo mariadb -e "ALTER USER root@localhost IDENTIFIED BY 'ifsp'"
+sudo mariadb -e "FLUSH PRIVILEGES"
+pip install git+https://github.com/caiohamamura/mysql_kernel
+python -m mysql_kernel.install
